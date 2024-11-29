dbeb4e1d6c6da112468b46bed8812e61d518e16f
mirzamahmudov007 <gayratjonuzbek07@gmail.com>
2024-11-29 10:51:18 +0500

    travel ended

--- src/components/our/Our.jsx
+++ src/components/our/Our.jsx
@@ -1,17 +1,26 @@
-import React from 'react'
-import './Our.css'
+import React from 'react';
+import './Our.css';
 import Img from './../../assets/pngwing.com 1.png';
 import Strelka from '../../assets/Vector.png';
 
 export default function Our() {
-  return (
-    <section className='our'>
-        <img className='our__img' src={Img} alt='images'/>
-        <div className='our__content'>
-              <h2 className='our__title'>Get Our Monthly Newsletter</h2>
-              <p className='our__text'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-              <div className='our__btn'><input type="text" placeholder=' Input your emaill address in here ' /><span className='our__strelka'><img src={Strelka}/></span></div>
-        </div>
-    </section>
-  )
+    return (
+        <section className="our">
+            <img className="our__img" src={Img} alt="Decorative background" />
+            <div className="our__content">
+                <h2 className="our__title">Get Our Monthly Newsletter</h2>
+                <p className="our__text">
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+                    industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
+                    scrambled it to make a type specimen book.
+                </p>
+                <div className="our__btn">
+                    <input type="text" placeholder="Input your email address in here" />
+                    <span className="our__strelka">
+                        <img src={Strelka} alt="Arrow icon" />
+                    </span>
+                </div>
+            </div>
+        </section>
+    );
 }
